a74c2d4e56ca3031723e975ab2b056406aab60d0
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2023-11-30 11:44:52 +0100

    created header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,24 @@
-function init(){
-    includeHTML()
+function init() {
+  includeHTML();
+}
+
+async function includeHTML() {
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
+    }
+  }
+  // activeMenu(); ****************** For later on - to activate the menu hover if the user is on the webside
 }
 
 
-async function includeHTML() {
-    let includeElements = document.querySelectorAll("[w3-include-html]");
-    for (let i = 0; i < includeElements.length; i++) {
-      const element = includeElements[i];
-      file = element.getAttribute("w3-include-html"); // "includes/header.html"
-      let resp = await fetch(file);
-      if (resp.ok) {
-        element.innerHTML = await resp.text();
-      } else {
-        element.innerHTML = "Page not found";
-      }
-    }
-    activeMenu();
-  }
+function openPopUpNav(){
+    let popupNav = document.getElementById("popupNav");
+    popupNav.classList.toggle("d_none");
+}
