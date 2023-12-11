71de1b50de1fb1c07c73b1e8a391485254b34dd7
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2023-12-11 10:14:25 +0100

    added JSDoc in Contacts

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,3 +1,8 @@
+/**
+ * This function is used to create contact informations on the right side.
+ *  
+ * @param {number} id - This id is the position of the Elements name
+ */
 function showName(id){
     document.getElementById('name1').classList.add('activeName');
     document.getElementById('contact_overview').classList.add('display_flex');
