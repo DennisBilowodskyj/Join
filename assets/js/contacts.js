fa29f3d73ef744516ecd39726819a7cda48896e6
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2023-12-11 10:07:28 +0100

    add button funktion on contacts

---
+++ assets/js/contacts.js
@@ -0,0 +1,10 @@
+function showName(id){
+    document.getElementById('name1').classList.add('activeName');
+    document.getElementById('contact_overview').classList.add('display_flex');
+}
+
+// function removeActiveName{
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//     }
+// }
