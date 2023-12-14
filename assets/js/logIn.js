746b9ecf81ada093c7492674d344ba4abbbad841
LarsThoennes <larsthoennes@gmail.com>
2023-12-14 21:21:00 +0100

    logIn.js and signUp.js merged into index.js

--- assets/js/logIn.js
+++ assets/js/logIn.js
@@ -1,14 +1,2 @@
-function checkInputLogIn(){
-    let inputIds = ['passwordLogIn'];
-    let imgIds = ['passwordLogInImg'];
 
-    inputIds.forEach((inputId, index) => {
-        let input = document.getElementById(inputId);
-        let img = document.getElementById(imgIds[index]);
-        if (input.value.trim() !== "") {
-            img.src = "assets/img/signUp_icons/hidePassword.png";
-        } else {
-            img.src = "assets/img/signUp_icons/lock.png";
-        }
-    });
-}
+
--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -93,3 +93,19 @@ function hidePassword(input, img) {
         showPassword(input, img);
     };
 }
+
+
+function checkInputLogIn(){
+    let inputIds = ['passwordLogIn'];
+    let imgIds = ['passwordLogInImg'];
+
+    inputIds.forEach((inputId, index) => {
+        let input = document.getElementById(inputId);
+        let img = document.getElementById(imgIds[index]);
+        if (input.value.trim() !== "") {
+            img.src = "assets/img/signUp_icons/hidePassword.png";
+        } else {
+            img.src = "assets/img/signUp_icons/lock.png";
+        }
+    });
+}
