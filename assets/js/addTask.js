6abe79af532290c7a179b5c8f647976cc1b5e56e
Viktor Ens <v.ens86@yahoo.de>
2023-12-16 10:59:48 +0100

    addTask Overlay changed small update

--- assets/js/addTask.js
+++ assets/js/addTask.js
@@ -77,7 +77,16 @@ function addSubtask() {
 
     let subtask = subtaskInput.value;
 
-    subtaskOutput.innerHTML += `<li>${subtask}</li>`;
+    subtaskOutput.innerHTML += /*html*/`
+       <div class="subtaskContainer d-flex">
+            <li>${subtask}</li>
+            <div class="d-flex">
+                <img src="./assets/img/addTask_icons/subtask_edit.png" alt="">
+                <div class="seperatorSubtask"></div>
+                <img src="./assets/img/addTask_icons/subtask_delete.png" alt="">
+            </div>
+       </div>
+    `;
     subtasks.push(subtask);
 
     subtaskInput.value = '';
@@ -130,14 +139,14 @@ function renderAssignedBadges() {
 function setPrio(buttonID) {
     taskPrio = buttonID;
     console.log(taskPrio);
-    
-    
+
+
 
     let btnUrgent = document.getElementById('prio_urgent');
     let btnMedium = document.getElementById('prio_medium');
     let btnLow = document.getElementById('prio_low');
     let prioBtn = [btnUrgent, btnMedium, btnLow]
-    
+
     prioBtn.forEach(btn => {
         btn.style.backgroundColor = 'white';
         btn.style.color = 'black';
@@ -145,30 +154,30 @@ function setPrio(buttonID) {
 
         let img = btn.querySelector('img');
         img.src = img.src.replace('_white.svg', '.svg');
-       
+
     });
 
-    if(taskPrio === 'urgent'){
+    if (taskPrio === 'urgent') {
         btnUrgent.style.backgroundColor = ('#FF3D00');
         btnUrgent.style.color = 'white';
         btnUrgent.style.boxShadow = '0px 4px 4px 0px #00000040';
         let img = btnUrgent.querySelector('img');
         img.src = './assets/img/addTask_icons/prio_urgent_white.svg';
-        
+
     } else if (taskPrio === 'medium') {
         btnMedium.style.backgroundColor = ('#FFA800');
         btnMedium.style.color = 'white';
         btnMedium.style.boxShadow = '0px 4px 4px 0px #00000040';
         let img = btnMedium.querySelector('img');
         img.src = './assets/img/addTask_icons/prio_medium_white.svg';
-        
-    }else if (taskPrio === 'low') {
+
+    } else if (taskPrio === 'low') {
         btnLow.style.backgroundColor = ('#7AE229');
         btnLow.style.color = 'white';
         btnLow.style.boxShadow = '0px 4px 4px 0px #00000040';
         let img = btnLow.querySelector('img');
         img.src = './assets/img/addTask_icons/prio_low_white.svg';
-        
+
     }
 
 
