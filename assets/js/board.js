747d25bf59a3e3b56e5aeb6bb9ea63a50c9f199e
Max Wirnsberger <maximilian.wirnsberger@gmail.com>
2023-12-16 17:00:19 +0100

    created info card on board

--- assets/js/board.js
+++ assets/js/board.js
@@ -3,3 +3,14 @@
 function openAddTaskOverlay() {
 document.getElementById('addTaskOverlay').classList.toggle("d-none");
 }
+
+function closeAddTaskOverlay() {
+    document.getElementById('addTaskOverlay').classList.toggle("d-none");
+}
+
+function closeCardDetails(){
+    document.getElementById('card_details_bg').classList.add("d-none");
+}
+
+// function openCardDetails(){
+// }
