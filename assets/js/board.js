144c73daec25a18fa8b0621fc08adc9fac28793f
Viktor Ens <v.ens86@yahoo.de>
2023-12-16 14:17:31 +0100

    board overlay transform

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,5 +1,11 @@
 
 
 function openAddTaskOverlay() {
-document.getElementById('addTaskOverlay').classList.toggle("d-none");
+// document.getElementById('addTaskOverlay').classList.toggle("d-none");
+document.getElementById('addTaskOverlay').style.transform = ('translateX(0)');
+
+}
+
+function closeAddTaskOverlay() {
+    document.getElementById('addTaskOverlay').style.transform = ('translateX(100%)');
 }
