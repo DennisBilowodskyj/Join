ddc5a7ad30881ff7c2427291c9906217eb6d905a
LarsThoennes <larsthoennes@gmail.com>
2023-12-17 01:29:00 +0100

    compress userSignUp()

--- assets/js/index.js
+++ assets/js/index.js
@@ -149,42 +149,76 @@ function checkEmail(users, emailToSearch) {
 
 async function userSignUp(){
     let emailErrorDiv = document.getElementById('emailErrorMessageSignUp');
-    document.getElementById('inputEmailDivRedBorderSignUp').classList.remove('check-password-red-border');
-    emailErrorDiv.innerHTML = '';
     let passwordErrorDiv = document.getElementById('passwordErrorMessageSignUp');
-    document.getElementById('inputPasswordDivRedBorderSignUp').classList.remove('check-password-red-border');
     let emailToSearch = document.getElementById('email');
+    removeRedBorderClassList();
+    emptyErrorDivText(emailErrorDiv, passwordErrorDiv);
     if (checkEmail(users, emailToSearch.value)) {
-        emailErrorDiv.innerHTML = 'Ups! Diese Email ist schon vergeben.';
-        document.getElementById('inputEmailDivRedBorderSignUp').classList.add('check-password-red-border');
-        return;
+        emailErrorDiv = setEmailAlert(emailErrorDiv);
     }
     if(password.value === checkPassword.value){
-        passwordErrorDiv.innerHTML = '';
-    signUpButton.disabled = true;
+        signUpButton.disabled = true;
+        pushIntoUsers();
+    await setItem('users', JSON.stringify(users));
+    deleteCurrentUser();
+    pushIntoCurrentUser();
+    localStorage.setItem('currentUser', JSON.stringify(currentUser));
+    keyframeAnimationSignUp();
+    loadIndex();
+    resetForm();
+    }else{
+        passwordErrorDiv = setPasswordAlert(passwordErrorDiv);
+    }
+}
+
+
+function removeRedBorderClassList(){
+    document.getElementById('inputEmailDivRedBorderSignUp').classList.remove('check-password-red-border');
+    document.getElementById('inputPasswordDivRedBorderSignUp').classList.remove('check-password-red-border');
+}
+
+
+function emptyErrorDivText(emailErrorDiv, passwordErrorDiv){
+    emailErrorDiv.innerHTML = '';
+    passwordErrorDiv.innerHTML = '';
+}
+
+
+function setEmailAlert(emailErrorDiv){
+    emailErrorDiv.innerHTML = 'Ups! Diese Email ist schon vergeben.';
+    document.getElementById('inputEmailDivRedBorderSignUp').classList.add('check-password-red-border');
+    return emailErrorDiv;
+}
+
+
+function pushIntoUsers(){
     users.push({
         name: userName.value,
         email: email.value,
         password: password.value,
         confirmPassword : checkPassword.value,
     });
-    await setItem('users', JSON.stringify(users));
+}
+
+
+function deleteCurrentUser(){
     while (currentUser.length > 0) {
         currentUser.pop(); 
     }
+}
+
+
+function pushIntoCurrentUser(){
     currentUser.push({
         currentEmail: email.value,
         currentPassword: password.value,
     });
-    localStorage.setItem('currentUser', JSON.stringify(currentUser));
+}
+
+
+function keyframeAnimationSignUp(){
     document.getElementById('body').classList.add('succes-sign-up');
     document.getElementById('keyframeSignUp').classList.add('start-animation');
-    loadIndex();
-    resetForm();
-    }else{
-        passwordErrorDiv.innerHTML = 'Ups! your password dont match';
-        document.getElementById('inputPasswordDivRedBorderSignUp').classList.add('check-password-red-border');
-    }
 }
 
 
@@ -202,6 +236,12 @@ function resetForm(){
     password.value = '';
     checkPassword.value = '';
     signUpButton.disabled = false;
+}
+
+
+function setPasswordAlert(passwordErrorDiv){
+    passwordErrorDiv.innerHTML = 'Ups! your password dont match';
+    document.getElementById('inputPasswordDivRedBorderSignUp').classList.add('check-password-red-border');
 }
 
 
