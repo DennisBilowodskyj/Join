ea0760d92aea399a28689d5af74b4ab56d3e1ffc
LarsThoennes <larsthoennes@gmail.com>
2023-12-18 00:41:07 +0100

    add localStorage to function goBack() and openNewTab()

--- assets/js/undersides.js
+++ assets/js/undersides.js
@@ -1,8 +1,14 @@
 function goBack() {
-    window.history.back();
-}
+    let previousPage = sessionStorage.getItem("previousPage");
+    if (previousPage) {
+      window.location.href = previousPage;
+    } else {
+      alert("Keine vorherige Seite gefunden");
+    }
+  }  
 
 
 function openNewTab(url) {
+    sessionStorage.setItem('previousPage', window.location.href);
     window.open(url, '_blank');
 }
