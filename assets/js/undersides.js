9d764034932da1504f6c547c530d882b0161b621
LarsThoennes <larsthoennes@gmail.com>
2023-12-18 09:22:51 +0100

    delte openNewTab()

--- assets/js/undersides.js
+++ assets/js/undersides.js
@@ -1,14 +1,3 @@
 function goBack() {
-    let previousPage = sessionStorage.getItem("previousPage");
-    if (previousPage) {
-      window.location.href = previousPage;
-    } else {
-      alert("Keine vorherige Seite gefunden");
-    }
-  }  
-
-
-function openNewTab(url) {
-    sessionStorage.setItem('previousPage', window.location.href);
-    window.open(url, '_blank');
-}
+    window.history.back();
+  }
